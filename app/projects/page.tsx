d5e02e1e31c8377a93852c0e1f372e563108a0e4
Riyan Maknojia <riyanmak123@gmail.com>
2026-04-07 19:17:12 -0500

    directories set up

---
+++ app/projects/page.tsx
@@ -0,0 +1,5 @@
+export default function Projects () {
+    return (
+        <div>These are the list of projects</div>
+    )
+}
---
+++ app/skills/page.tsx
@@ -0,0 +1,7 @@
+//import the terminal component here
+
+export default function Skills() {
+    return (
+        <div>Here are my skills</div>
+    )
+}
