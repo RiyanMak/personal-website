1a8828a229939f2100c610080ded20ec9749da67
Riyan Maknojia <riyanmak123@gmail.com>
2026-04-21 22:28:55 -0500

    introduction content

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="center"></div>
+    <div className="p-60 text-7xl font-poppins font-extrablack">Riyan Maknojia
+      <div className="p-5 text-3xl">Hello! I'm a Student
+        <div>Software Engineer</div>
+        <div>and a Martial Artist</div>
+      </div>
+    </div>
   );
 }
